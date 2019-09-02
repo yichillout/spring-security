e9d3c14b7aab335dc1a3abd3c59dcb429275179f
jasper <jasper233xd@gmail.com>
2019-09-02 11:37:30 -0400

    updated SecurityDemoAuthenctiationFailureHandler and SecurityDemoAuthenticationSuccessHandler

--- jasper-security-browser/src/main/java/com/jasper/security/browser/authentication/SecurityDemoAuthenctiationFailureHandler.java
+++ jasper-security-browser/src/main/java/com/jasper/security/browser/authentication/SecurityDemoAuthenctiationFailureHandler.java
@@ -16,35 +16,36 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jasper.security.browser.support.SimpleResponse;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import com.jasper.security.core.properties.LoginResponseType;
 import com.jasper.security.core.properties.SecurityProperties;
 
 @Component("SecurityDemoAuthenctiationFailureHandler")
-public class SecurityDemoAuthenctiationFailureHandler implements AuthenticationFailureHandler {
+public class SecurityDemoAuthenctiationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
-	
+
 	@Autowired
 	private SecurityProperties securityProperties;
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException exception) throws IOException, ServletException {
-		
 		logger.info("login failure");
-		
-//		if (LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
+
+		if (LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginResponseType())) {
 			response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 			response.setContentType("application/json;charset=UTF-8");
-//			response.getWriter().write(objectMapper.writeValueAsString(new SimpleResponse(exception.getMessage())));
+			// response.getWriter().write(objectMapper.writeValueAsString(new
+			// SimpleResponse(exception.getMessage())));
 			response.getWriter().write(objectMapper.writeValueAsString(exception));
-//		}else{
-//			super.onAuthenticationFailure(request, response, exception);
-//		}
-		
-		
+		} else {
+			super.onAuthenticationFailure(request, response, exception);
+		}
 	}
 
 }
--- jasper-security-browser/src/main/java/com/jasper/security/browser/authentication/SecurityDemoAuthenticationSuccessHandler.java
+++ jasper-security-browser/src/main/java/com/jasper/security/browser/authentication/SecurityDemoAuthenticationSuccessHandler.java
@@ -11,29 +11,35 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jasper.security.core.properties.LoginResponseType;
+import com.jasper.security.core.properties.SecurityProperties;
 
 @Component("SecurityDemoAuthenticationSuccessHandler")
-public class SecurityDemoAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+public class SecurityDemoAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	private ObjectMapper objectMapper;
 
+	@Autowired
+	private SecurityProperties securityProperties;
+
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		logger.info("login success");
 
-//		if (LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
+		if (LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginResponseType())) {
 			response.setContentType("application/json;charset=UTF-8");
 			response.getWriter().write(objectMapper.writeValueAsString(authentication));
-//		} else {
-//			super.onAuthenticationSuccess(request, response, authentication);
-//		}
+		} else {
+			super.onAuthenticationSuccess(request, response, authentication);
+		}
 	}
 
 }
